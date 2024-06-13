2e3d1db5660adce7770381e0b05ab501622fddc1
MihaelaO29 <opriamihaela22@yahoo.com>
2024-06-13 20:30:08 +0300

    added reservation section

--- src/components/pages/reservation/reservation.tsx
+++ src/components/pages/reservation/reservation.tsx
@@ -1,29 +1,51 @@
 import './reservation.css';
+import Select from 'react-select';
 
 
 
 function Reservation() {
+    const options = [
+        { value: 'New York', label: 'Select event New York' },
+        { value: 'Barcelona', label: 'Select event Barcelona' },
+        { value: 'Ibiza', label: 'Select event Ibiza' }
+    ]
+
     return (
         <div className='reservation_section'>
-            <div>Make Reservation</div>
-            <div>
-                <div>
-                    <input
-                        type='text'
-                        placeholder='Enter your name'
-                    />
-                    <input
-                        type='email'
-                        placeholder='Enter email address'
-                    />
-                    <input
-                        type='number'
-                        placeholder='Phone number'
-                    />
+            <div className='reservation_title'>Make Reservation</div>
+            <div className='reservation_form'>
+                <div className='form'>
+                    <div style={{ display: 'flex', flexDirection: 'column' }}>
+                        <div >
+                            <input className='reservation_input'
+                                type='text'
+                                placeholder='Enter your name'
+                            />
+                        </div>
+                        <div>
+                            <input className='reservation_input'
+                                type='email'
+                                placeholder='Enter email address'
+                            />
+                        </div>
+                        <div>
+                            <input className='reservation_input'
+                                type='tel'
+                                placeholder='Phone number'
+                            />
+                        </div>
+                        <div>
+                            <input className='reservation_input'
+                                type='text'
+                                placeholder='City'
+                            />
+                        </div>
 
+                        <Select options={options} />
+                    </div>
+                    <button className='reservation_btn'>Make Reservation</button>
                 </div>
             </div>
-
         </div>
     );
 }
